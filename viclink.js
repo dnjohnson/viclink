e80bdb48aa383a94c4cf8e63670efa377a2aac0b
David N. Johnson <me@dnj.io>
2014-05-05 20:46:20 +1000

    Adds basic PTV connectivity

--- viclink.js
+++ viclink.js
@@ -1,9 +1,19 @@
+/*
+	Recuired environment variables:
+
+	http://www.data.vic.gov.au/raw_data/ptv-timetable-api/6056
+	PTV_DEVID: Public Transport Victoria Developer ID
+	PTV_KEY: Public Transport Victoria Secret Key
+*/
+
 var http = require("http");
 var jade = require("jade");
 var stylus = require("stylus");
 var fs = require("fs");
 var ecstatic = require("ecstatic")(__dirname + "/build");
 var router = require("routes")();
+var request = require("request");
+var crypto = require("crypto");
 
 var routes = { };
 
@@ -29,6 +39,18 @@ routes.styles = function (req, res) {
 router.addRoute("/", routes.render);
 router.addRoute("/*.css", routes.styles);
 
+router.addRoute("/healthcheck", function (req, res) {
+	var sig = sign("/v2/healthcheck?devid=" + process.env.PTV_DEVID);
+
+	request({
+		url: "http://timetableapi.ptv.vic.gov.au/v2/healthcheck",
+		qs: {
+			devid: process.env.PTV_DEVID,
+			signature: sig
+		}
+	}).pipe(res);
+});
+
 var viclink = http.createServer(function (req, res) {
 	var route = router.match(req.url);
 
@@ -40,5 +62,18 @@ var viclink = http.createServer(function (req, res) {
 		ecstatic(req, res);
 	}
 });
+
+function sign(url) {
+	//console.log("URL", url);
+	//console.log("ID", process.env.PTV_DEVID);
+	//console.log("KEY", process.env.PTV_KEY);
+
+	var hmac = crypto.createHmac("sha1", process.env.PTV_KEY);
+	hmac.setEncoding("hex");
+	hmac.write(url);
+	hmac.end();
+
+	return hmac.read().toUpperCase();
+}
 
 viclink.listen(8000);
